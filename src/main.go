ab3cec456b7f43536cdf1a9b4694257d2fbf4692
ju zi <juzi90990@gmail.com>
2024-04-06 00:35:52 +0700

    证书配置

--- src/main.go
+++ src/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "flag"
-import . "webClip/api"
+import (
+	"flag"
+	. "webClip/api"
+)
 
 func main() {
-	var ip = flag.String("web", "../public", "please input you web root")
-	var c = flag.String("crt", "", "please input you  crt path")
-	var k = flag.String("key", "", "please input you  key path")
-	var u = flag.String("ca", "", "please input you  ca path")
+	var ip = flag.String("web", "./public", "please input you web root")
+	var c = flag.String("crt", "./webclip-crt.pem", "please input you  crt path")
+	var k = flag.String("key", "./webclip-key.pem", "please input you  key path")
+	var u = flag.String("ca", "./webclip-ca.pem", "please input you  ca path")
 	flag.Parse()
 	Run(*ip, *c, *k, *u)
 }
